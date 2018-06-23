023fa2b0a65fb0bd3b2dd574ffe5b7236fb3e615
DarkMagician <omermagdy100@gmail.com>
2018-06-23 02:26:49 +0200

    Update Program.py

--- post-process/Program.py
+++ post-process/Program.py
@@ -1,9 +1,14 @@
 from Server import *
+import time
 MyServer=Server()
 MyServer.newConnection()
 MyServer.getRequiredActions()
 while True:
+    t0 = time.time()
     frame= MyServer.getNewFrame()
     objects= MyServer.detectObjects(frame)
     Alarms= MyServer.detectActions(objects)
     MyServer.SendAlarm(Alarms)
+    t1 = time.time()
+    print(t1-t0)
+
